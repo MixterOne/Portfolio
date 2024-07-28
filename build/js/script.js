6781a4aeb096b1c3af8d9ecaaaa6701c91174518
MixterOne <matheusisaias879@gmail.com>
2024-07-28 09:53:12 -0300

    adicionando section dos projetos e suas funcionalidades

--- build/js/script.js
+++ build/js/script.js
@@ -25,3 +25,36 @@ const write = () => {
 }
 
 write();
+
+function showTab(tabId) {
+    const tabs = document.querySelectorAll('.tab_content');
+    tabs.forEach(tab => {
+        tab.style.display = 'none';
+    });
+
+    document.getElementById(tabId).style.display = 'block';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('modal');
+    const modalContent = document.querySelector('.modal_body');
+    const closeModal = document.querySelector('.modal .close');
+
+    document.querySelectorAll('.open_modal').forEach(button => {
+        button.addEventListener('click', (event) => {
+            const modalInfo = event.target.closest('.info').querySelector('.modal_info').innerHTML;
+            modalContent.innerHTML = modalInfo;
+            modal.style.display = 'block';
+        });
+    });
+
+    closeModal.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+});
